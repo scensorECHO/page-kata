44c9fd31f4f02a82c7b3f2c27bb51bb407f091d8
Tom <scensorECHO@users.noreply.github.com>
2015-09-22 14:19:19 -0400

    Added code from kata

---
+++ PaginationHelper.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+public class PaginationHelper<I> {
+  
+  private List<I> collection;
+  private int itemsPerPage;
+  private int pages;
+  
+  /**
+   * The constructor takes in an array of items and a integer indicating how many
+   * items fit within a single page
+   */
+  public PaginationHelper(List<I> collection, int itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+    pages = itemCount()/itemsPerPage;
+  }
+  
+  /**
+   * returns the number of items within the entire collection
+   */
+  public int itemCount() {
+    return collection.size();
+  }
+  
+  /**
+   * returns the number of pages
+   */
+  public int pageCount() {
+    return pages + 1;
+  }
+  
+  /**
+   * returns the number of items on the current page. page_index is zero based.
+   * this method should return -1 for pageIndex values that are out of range
+   */
+  public int pageItemCount(int pageIndex) {
+    if(pageIndex>pages || pageIndex<0)
+      return -1;
+    return pageIndex!=pages
+      ? itemsPerPage
+      : itemCount()%itemsPerPage;
+  }
+  
+  /**
+   * determines what page an item is on. Zero based indexes
+   * this method should return -1 for itemIndex values that are out of range
+   */
+  public int pageIndex(int itemIndex) {
+    if(itemIndex > itemCount() || itemIndex < 0 || itemCount()<=0)
+      return -1;
+    return itemIndex / itemsPerPage;
+  }
+}
